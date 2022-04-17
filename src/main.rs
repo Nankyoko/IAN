6e08d24575ab244995ddc0ae7211b44898bc8086
soliel <rachel@soltech.dev>
2022-04-17 00:35:37 -0400

    Learning Actix-Web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+//http://localhost/
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("<H1>HELLO WORLD!</H1>")
 }
